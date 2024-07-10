a0afb2b63bdd14378a8d885f369d82ce3d5e551a
Vyshnav C J <vyshnavjenilkumar@gmail.com>
2024-07-10 12:59:41 +0530

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,9 +11,15 @@ set -U fish_greeting
 # path
 fish_add_path $HOME/.local/share/bin
 fish_add_path /usr/local/go/bin
+# fish_add_path $HOME/.dotnet
+# fish_add_path $HOME/.dotnet/tools
 
 # git keybindings
 source ~/.config/fish/git.fish
 
 # fish shell intergration
 starship init fish | source
+
+# defaults
+# set -gx BROWSER /usr/bin/wslview
+set -Ux DOTNET_ROOT $HOME/.dotnet
