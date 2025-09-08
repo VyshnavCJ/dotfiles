aaadeee1073861c98f38e6a9824544275ae3cf0a
VyshnavCJ <vyshnavjenilkumar@gmail.com>
2025-09-08 19:22:45 +0530

    changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,20 @@
 # alias
 alias nv=nvim
-alias tmux="tmux -u"
-abbr --add ext cd /run/media/cj/workstation
-abbr --add dev cd /run/media/cj/workstation/dev
-abbr --add work cd /run/media/cj/workstation/work
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -Ux TMUX_CONF $HOME/.config/tmux/tmux.conf
+alias tmux "tmux -u"
+abbr --add dev cd /workstation/dev
+abbr --add work cd /workstation/dev/work
 bind \ef 'tmux-sessionizer'
 
 
 set -U fish_greeting
-
+:
 # path
 fish_add_path -U $HOME/.local/share/bin
 fish_add_path -U /usr/local/go/bin
 fish_add_path -U $HOME/.dotnet
+fish_add_path -U $HOME/go/bin
 fish_add_path -U $HOME/.dotnet/tools
 
 # git keybindings
---
+++ scripts/restore_windows.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+sleep 10
+
+# Monitor position
+MONITOR_LEFT_X=0
+MONITOR_LEFT_Y=0
+MONITOR_RIGHT_X=5120
+MONITOR_RIGHT_Y=0
+
+wmctrl -lx | grep "chatgpt" | awk '{print $1}' | while read wid; do
+  wmctrl -ir $wid -t 0
+  xdotool windowmove $wid $MONITOR_RIGHT_X $MONITOR_RIGHT_Y
+  wmctrl -ir $wid -b add,maximized_vert,maximized_horz
+done
+
+wmctrl -lx | grep "thunar.Thunar" | awk '{print $1}' | while read wid; do
+  wmctrl -ir $wid -t 0
+  xdotool windowmove $wid $MONITOR_RIGHT_X $MONITOR_RIGHT_Y
+  wmctrl -ir $wid -b add,maximized_vert,maximized_horz
+done
+
+wmctrl -lx | grep "org.wezfurlong.wezterm.org.wezfurlong.wezterm" | awk '{print $1}' | while read wid; do
+  wmctrl -ir $wid -t 0
+  xdotool windowmove $wid $MONITOR_LEFT_X $MONITOR_LEFT_Y
+  wmctrl -ir $wid -b add,maximized_vert,maximized_horz
+done
+
+wmctrl -lx | grep "vesktop.vesktop" | awk '{print $1}' | while read wid; do
+  wmctrl -ir $wid -t 1
+  xdotool windowmove $wid $MONITOR_RIGHT_X $MONITOR_RIGHT_Y
+  wmctrl -ir $wid -b add,maximized_vert,maximized_horz
+done
+
+wmctrl -lx | grep "todoist-nativefier-a2ca46.todoist-nativefier-a2ca46" | awk '{print $1}' | while read wid; do
+  wmctrl -ir $wid -t 1
+  xdotool windowmove $wid $MONITOR_RIGHT_X $MONITOR_RIGHT_Y
+  wmctrl -ir $wid -b add,maximized_vert,maximized_horz
+done
+
+wmctrl -lx | grep "Navigator.firefox" | awk '{print $1}' | while read wid; do
+  wmctrl -ir $wid -t 1
+  xdotool windowmove $wid $MONITOR_LEFT_X $MONITOR_LEFT_Y
+  wmctrl -ir $wid -b add,maximized_vert,maximized_horz
+done
+
+wmctrl -lx | grep "mastodon-nativefier-389aa3.mastodon-nativefier-389aa3" | awk '{print $1}' | while read wid; do
+  wmctrl -ir $wid -t 2
+  xdotool windowmove $wid $MONITOR_LEFT_X $MONITOR_LEFT_Y
+  wmctrl -ir $wid -b add,maximized_vert,maximized_horz
+done
+
+wmctrl -lx | grep "Mail.thunderbird-esr" | awk '{print $1}' | while read wid; do
+  wmctrl -ir $wid -t 2
+  xdotool windowmove $wid $MONITOR_RIGHT_X $MONITOR_RIGHT_Y
+  wmctrl -ir $wid -b add,maximized_vert,maximized_horz
+done
+
+wmctrl -lx | grep "obsidian.obsidian" | awk '{print $1}' | while read wid; do
+  wmctrl -ir $wid -t 3
+  xdotool windowmove $wid $MONITOR_LEFT_X $MONITOR_LEFT_Y
+  wmctrl -ir $wid -b add,maximized_vert,maximized_horz
+done
+
